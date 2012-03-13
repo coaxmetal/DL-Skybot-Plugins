cf5e9b11f3fe2255958b5cae4b6f5e552b031780
Derek Leverenz <derek.leverenz@gmail.com>
2012-03-12 19:29:23 -0700

    should be working now

--- beeradvocate.py
+++ beeradvocate.py
@@ -6,7 +6,7 @@ def beeradvocate(inp):
     """ search beeradvocate.com """
 
     search_url = 'http://beeradvocate.com/search'
-    base_url = 'http://beeradvocate.com/'
+    base_url = 'http://beeradvocate.com'
 
     results = http.get_html(search_url, q=http.quote_plus(inp))
     result = results.xpath("//td[@id='mainContent']/div[2]/ul/li[1]")
@@ -14,8 +14,7 @@ def beeradvocate(inp):
     if not result:
         return 'No Results'
     else:
-        page_url = base_url + results[0].get('href')
-        page = http.get_html(page)
-        community_score = page.xpath("//td[@id='mainContent']/table[1]/tbody/tr[1]/td[1]/table/tbody/tr/td[1]/span[@class='BAscore_big']")
-        bros_score = page.xpath("//td[@id='mainContent']/table[1]/tbody/tr[1]/td[1]/table/tbody/tr/td[2]/span[@class='BAscore_big']")
-        return results[0].text_content() + ' || ' + 'Community Score: %s, ' % community_score.text_content() + 'Bros score: %s' % bros_score + ' | ' + page_url
+        page_url = base_url + result[0].xpath('a')[0].get('href')
+        page = http.get_html(page_url)
+        scores = page.cssselect('.BAscore_big')
+        return result[0].text_content() + ' || ' + 'Community Score: %s, ' % scores[0].text_content() + 'Bros score: %s' % scores[1].text_content()+ ' | ' + page_url
