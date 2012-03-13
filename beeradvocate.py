492c402e843debb17093e9af64e38a066b76e977
Derek Leverenz <derek.leverenz@gmail.com>
2012-03-12 19:57:30 -0700

    same thing, but cleaned up and nicer output formatting, thanks to ell

--- beeradvocate.py
+++ beeradvocate.py
@@ -1,20 +1,28 @@
 from util import hook, http
 
-@hook.command('beer')
-@hook.command
-def beeradvocate(inp):
+
+def get_beer(beer):
     """ search beeradvocate.com """
 
-    search_url = 'http://beeradvocate.com/search'
-    base_url = 'http://beeradvocate.com'
+    search_url = "http://beeradvocate.com/search"
+    base_url = "http://beeradvocate.com"
 
     results = http.get_html(search_url, q=http.quote_plus(inp))
-    result = results.xpath("//td[@id='mainContent']/div[2]/ul/li[1]")
+    
+    try:
+        result = results.xpath("//td[@id='mainContent']/div[2]/ul/li[1]")[0]
+    except IndexError:
+        return "No Results"
+    
+    page_url = base_url + result.xpath('a')[0].get('href')
+    scores = http.get_html(page_url).cssselect('.BAscore_big')
+    return "%s :: Community Score: %s :: Bros Score: %s :: %s" % (result.text_content(), 
+                                                                  scores[0].text_content(), 
+                                                                  scores[1].text_content(), page_url)
 
-    if not result:
-        return 'No Results'
-    else:
-        page_url = base_url + result[0].xpath('a')[0].get('href')
-        page = http.get_html(page_url)
-        scores = page.cssselect('.BAscore_big')
-        return result[0].text_content() + ' || ' + 'Community Score: %s, ' % scores[0].text_content() + 'Bros score: %s' % scores[1].text_content()+ ' | ' + page_url
+@hook.command
+def beeradvocate(inp):
+    return get_beer(inp)
+
+if __name__ == "__main__":
+    print get_beer("budweiser")
